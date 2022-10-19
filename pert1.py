4b02e2bf51b805ccd29cf4e44e26a0d3b29cf55f
nizar202108010373 <nizaralamsyah21@gmail.com>
2022-10-19 18:42:07 +0700

    pertemuan1

---
+++ pert1.py
@@ -0,0 +1,20 @@
+a=20210801373
+b="Nizar Hilmi Alaudin"
+
+c="sample operator in python"
+print(a,b)
+print(c)
+nilai1=int(input("nilai 1 = "))
+nilai2=int(input("nilai 2 = "))
+penjumlahan=nilai1+nilai2
+
+pengurangan=nilai1-nilai2
+
+perkalian=nilai1*nilai2
+
+pembagian=nilai1/nilai2
+
+print("nilai penjumlahan ", penjumlahan)
+print("nilai pengurangan", pengurangan)
+print("nilai perkalian", perkalian)
+print("nilai pembagian", pembagian)
